535db8920fffb68bc98a6980743645f0dbd8abe6
m.sinans <msomercikoglu@gmail.com>
2024-10-05 23:26:36 +0300

    Karar Yapıları If Else

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // if bir karar yapısıdır.
+            // Karar yapılarından sonra ; kullanılmaz. Çünkü cümleye başlamış ama henüz bitirmemişizdir. (Eğer şöyleyse...)
+            // == operatörü eşit mi? anlamaındadır.
+
+            #region If Else
+
+            Console.Write("Lütfen Şifreyi Girinz: ");
+            string password;
+            password = Console.ReadLine();
+
+            if (password== "abcd")
+            {
+                Console.WriteLine("Şifre Doğru");
+            }
+            else
+            {
+                Console.WriteLine("Şifre Yanlış");
+            }
+
+            Console.Read();
+
+
+            #endregion
+        }
+    }
+}
